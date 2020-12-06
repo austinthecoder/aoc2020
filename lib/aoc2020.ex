aebe6e6cb024af0a97cfadf18f16192f1058341b
Austin Schneider <me@austinschneider.com>
2020-12-06 09:21:02 -0500

    refactor

--- lib/aoc2020.ex
+++ lib/aoc2020.ex
@@ -1,22 +1,18 @@
 defmodule Aoc2020 do
   def determine_expense_report_magic_number(expense_report_path, count) do
-    entries =
-      File.read!(expense_report_path)
-      |> String.split()
-      |> Enum.map(&String.to_integer/1)
-
-    found_entries =
-      find_entries(entries, count)
-      |> List.first()
-
-    Enum.reduce(found_entries, fn entry, acc -> entry * acc end)
+    File.read!(expense_report_path)
+    |> String.split()
+    |> Enum.map(&String.to_integer/1)
+    |> find_entry_lists_that_sum_to_2020(count)
+    |> List.first()
+    |> Enum.reduce(&(&1 * &2))
   end
 
-  defp find_entries(entries, 2) do
+  defp find_entry_lists_that_sum_to_2020(entries, 2) do
     for a <- entries, b <- entries, a + b == 2020, do: [a, b]
   end
 
-  defp find_entries(entries, 3) do
+  defp find_entry_lists_that_sum_to_2020(entries, 3) do
     for a <- entries, b <- entries, c <- entries, a + b + c == 2020, do: [a, b, c]
   end
 end
--- test/aoc2020_test.exs
+++ test/aoc2020_test.exs
@@ -4,13 +4,11 @@ defmodule Aoc2020Test do
 
   test "day 1, part 1" do
     answer = Aoc2020.determine_expense_report_magic_number("test/expense_report.txt", 2)
-
     assert answer == 996_996
   end
 
   test "day 1, part 2" do
     answer = Aoc2020.determine_expense_report_magic_number("test/expense_report.txt", 3)
-
     assert answer == 9_210_402
   end
 end
