8fee13d320c12efd6dfb44bd9b4f2c628c425ba3
Austin Schneider <me@austinschneider.com>
2020-12-08 10:22:02 -0500

    refactor

--- lib/aoc2020/bag_rules.ex
+++ lib/aoc2020/bag_rules.ex
@@ -1,7 +1,7 @@
 defmodule Aoc2020.BagRules do
   def from_batch(batch) do
     String.split(batch, "\n")
-    |> Enum.reduce(%{}, &add_rule/2)
+    |> Enum.reduce(%{}, &populate_rules/2)
   end
 
   def count_ancestors(rules, color) do
@@ -10,8 +10,8 @@ defmodule Aoc2020.BagRules do
   end
 
   def count_descendants(rules, color) do
-    sum_descendants = fn [count, containing_color], sum ->
-      sum + count + (count * count_descendants(rules, containing_color))
+    sum_descendants = fn {containing_color, count}, sum ->
+      sum + count + count * count_descendants(rules, containing_color)
     end
 
     Enum.reduce(rules[color], 0, sum_descendants)
@@ -20,23 +20,23 @@ defmodule Aoc2020.BagRules do
   ##########
 
   defp can_contain?(rules, color, target_color) do
-    contain? = fn [_, containing_color] ->
+    contain? = fn {containing_color, _count} ->
       containing_color == target_color || can_contain?(rules, containing_color, target_color)
     end
 
     Enum.any?(rules[color], contain?)
   end
 
-  defp add_rule(line, map) do
+  defp populate_rules(line, map) do
     [color] = Regex.run(~r/\A([\w\s]+)\sbags\scontain/, line, capture: :all_but_first)
 
-    to_containing_rule = fn [digits, color] ->
-      [String.to_integer(digits), color]
+    populate_containing_rules = fn [digits, color], map ->
+      Map.put(map, color, String.to_integer(digits))
     end
 
     containing_rules =
       Regex.scan(~r/(\d+)\s([\w\s]+)\sbag/, line, capture: :all_but_first)
-      |> Enum.map(to_containing_rule)
+      |> Enum.reduce(%{}, populate_containing_rules)
 
     Map.put(map, color, containing_rules)
   end
